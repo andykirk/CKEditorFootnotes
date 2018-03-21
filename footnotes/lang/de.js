8dd52ed79ab67249ad17d38443df2f1c29cbfe97
Lukas Weiss <lukas.weiss@raiffeisen.it>
2018-03-21 14:48:05 +0100

    Add language files for english and german

---
+++ footnotes/lang/de.js
@@ -0,0 +1,17 @@
+CKEDITOR.plugins.setLang('footnotes', 'de', {
+    buttons: {
+        add: 'Fußnote einfügen'
+    },
+    dialog: {
+        title: 'Fußnoten verwalten',
+        basicSettings: 'Einstellungen',
+        fields: {
+            text: 'Neue Fußnote:',
+            id: 'Keine Fußnoten vorhanden'
+        },
+        texts: {
+            or: 'ODER',
+            choose: 'vorhandene Fußnote verwenden:'
+        }
+    }
+});
